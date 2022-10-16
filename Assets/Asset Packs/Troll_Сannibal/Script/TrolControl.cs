54b9d6b3be5c5330c7e1773812dc34dbe7cf07cc
Ragnarok <s3rg10.martinez.roman@gmail.com>
2022-10-17 00:37:02 +0200

    - NPC Troll Cannibal

---
+++ Assets/Asset Packs/Troll_Сannibal/Script/TrolControl.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class TrolControl : MonoBehaviour {
+	
+	private Animator anim;
+	private CharacterController controller;
+
+
+	// Use this for initialization
+	void Start () {
+
+		anim = GetComponent<Animator>();
+		controller = GetComponent<CharacterController> ();
+	}
+}
