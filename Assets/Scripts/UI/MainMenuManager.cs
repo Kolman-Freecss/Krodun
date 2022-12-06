d1031de8d3dad924abbcaf49b4123e6b7be3e1a4
Kolman-Freecss <s3rg10.martinez.roman@gmail.com>
2022-12-06 17:28:29 +0100

    - WIP Main Menu Scene

--- Assets/Scripts/UI/MainMenuManager.cs
+++ Assets/Scripts/UI/MainMenuManager.cs
@@ -1,4 +1,7 @@
+using System;
+using System.Collections;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 namespace Kolman_Freecss.Krodun
 {
@@ -6,6 +9,7 @@ namespace Kolman_Freecss.Krodun
     {
         private GameObject _mainMenuManager;
         private static MainMenuManager Instance { get; set; }
+        private GameObject _multiplayerCanvas;
 
         private void Awake()
         {
@@ -13,8 +17,42 @@ namespace Kolman_Freecss.Krodun
             {
                 _mainMenuManager = GameObject.FindGameObjectWithTag("MainMenuManager");
             }
+            
+            if (_multiplayerCanvas == null)
+            {
+                _multiplayerCanvas = GameObject.FindGameObjectWithTag("MultiplayerCanvas");
+            }
 
             ManageSingleton();
+        }
+
+        private void Start()
+        {
+            _multiplayerCanvas.SetActive(false);
+        }
+
+        public void OnApplicationQuit()
+        {
+            Application.Quit();
+        }
+        
+        public void OnMultiplayerPressed()
+        {
+            _multiplayerCanvas.SetActive(true);
+        }
+        
+        public void OnStartButtonPressed()
+        {
+            StartCoroutine(LoadGame());
+        }
+        
+        IEnumerator LoadGame()
+        {
+            AsyncOperation asyncLoad = SceneManager.LoadSceneAsync("Kolman");
+            while (!asyncLoad.isDone)
+            {
+                yield return null;
+            }
         }
 
         void ManageSingleton()
