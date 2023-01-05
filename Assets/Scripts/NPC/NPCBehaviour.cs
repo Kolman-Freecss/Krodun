f034d340909096348bb7f10cfdfe8832666d763b
Kolman-Freecss <s3rg10.martinez.roman@gmail.com>
2023-01-05 16:32:00 +0100

    - EnemyTroll update - NPC's animations - Game over window updated

---
+++ Assets/Scripts/NPC/NPCBehaviour.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Kolman_Freecss.Krodun;
+using UnityEngine;
+
+namespace NPC
+{
+    public class NPCBehaviour : MonoBehaviour
+    {
+        private Animator _animator;
+        private bool _hasAnimator;
+        
+        private int _animIDIdle;
+        
+        void Start()
+        {
+            _hasAnimator = TryGetComponent(out _animator);
+            if (!_hasAnimator)
+            {
+                _animator = GetComponentInChildren<Animator>();
+                if (_animator != null)
+                {
+                    _hasAnimator = true;
+                }
+            }
+            AssignAnimationIDs();
+        }
+
+        private void Update()
+        {
+            if (_hasAnimator && !GameManager.Instance.isGameOver.Value)
+            {
+                _animator.SetBool(_animIDIdle, true);
+            } else if (_hasAnimator && GameManager.Instance.isGameOver.Value)
+            {
+                _animator.SetBool(_animIDIdle, false);
+            }
+        }
+
+        private void AssignAnimationIDs()
+        {
+            _animIDIdle = Animator.StringToHash("Idle");
+        }
+    }
+}
